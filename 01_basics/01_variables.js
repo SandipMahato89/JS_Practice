7c4f9909fa747464b6c1dc4a1322f6f858cc06ea
Sandip Mahato <sing91816@gmail.com>
2024-12-10 23:24:25 +0530

    Variables basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 34569;
+
+let name = "Sandip Mahato";
+
+var email = "sandip.email.com";
+
+// accountId = 23; not allowed
+console.log(accountId);
+/*
+we use only const and let but we do not use var due to scope issue in block and functional scope
+*/
+console.table([accountId, name, email]);
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,3 @@
 console.log("Hello Bhaiaya");
+
+console.log();
